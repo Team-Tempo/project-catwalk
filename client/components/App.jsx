b4281feebe767ec9e655ade78ad8d2eafcf4ea8a
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-04-13 16:50:11 -0500

    Initial app setup

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <button className="btn btn-primary">Sign Up!</button>
+    </>
+  );
+};
+
+export default App;
