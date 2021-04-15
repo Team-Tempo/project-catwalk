18b001e699a51f2e85fa84f8a5b46f207a38c864
Julia Thea <yuliya.boyadjan@gmail.com>
2021-04-15 15:37:00 -0500

    Added Reviews components

---
+++ client/components/RatingsAndReviews/Reviews/Review.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Review = () => {
+  return <h2>Hello from Review</h2>
+}
+
+export default Review;
---
+++ client/components/RatingsAndReviews/Reviews/ReviewsList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Review from './Review.jsx';
+
+const ReviewsList = () => {
+  return (
+    <>
+    <h1>Hello from ReviewList</h1>
+    <Review />
+    </>
+    )
+}
+
+export default ReviewsList;
