042795fd1687a05e2cdece783bb0f059e93748a9
Julia Thea <yuliya.boyadjan@gmail.com>
2021-04-15 15:37:48 -0500

    Lined up all the components

--- client/components/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import ReviewsList from './Reviews/ReviewsList.jsx'
+import Rating from './Rating/Rating.jsx'
 
 const RatingsAndReviews = () => {
-  return null;
+  return (
+    <>
+    <h1>This is Ratings and Reviews</h1>
+    <Rating />
+    <ReviewsList />
+    </>
+  )
 };
 
 export default RatingsAndReviews;
