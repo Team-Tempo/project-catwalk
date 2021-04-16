203ba40eeed72c2226c6bd19f5a2a7263152ec36
Amanda Davitt <acdavitt@gmail.com>
2021-04-16 16:02:10 -0500

    Updated product card to match design template; still with dummy data

--- client/components/RelatedProducts/Related/RelatedProductCard.jsx
+++ client/components/RelatedProducts/Related/RelatedProductCard.jsx
@@ -1,26 +1,77 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 import products from '../../DummyData/ProductDummyData';
-import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Rating from '@material-ui/lab/Rating';
 
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 250,
+    minHeight: 300,
+    margin: 16
+  },
+  category: {
+    fontSize: 12,
+    textTransform: 'uppercase'
+  },
+  name: {
+    fontWeight: 700
+  },
+  price: {
+    fontSize: 12
+  },
+  media: {
+    height: 250,
+    position: 'relative'
+  },
+  overlay: {
+    position: 'absolute',
+    top: '1px',
+    right: '1px',
+    color: '#ffb400'
+  }
+});
 
 
 const RelatedProductCard = (props) => {
+  const classes = useStyles();
 
   return (
-    <Card>
-      <img src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2xvdGhpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" width="100" height="150"></img>
-      {/* action button
-      image
-      category
-      name
-      default_price
-      sale_price if on sale
-      star rating */}
-      <div>{props.relatedWithNameCatPrice.category}</div>
-      <div>{props.relatedWithNameCatPrice.name}</div>
-      <div>{props.relatedWithNameCatPrice.default_price}</div>
+    <Card className={classes.root}>
+      <CardActionArea >
+        <CardMedia
+        className={classes.media}
+        image="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2xvdGhpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        />
+        <IconButton className={classes.overlay}>
+          <span className="material-icons">star_rate</span>
+        </IconButton>
+        <CardContent>
+          {/*
+          sale_price if on sale
+          */}
+          <Typography className={classes.category}>
+            {props.relatedWithNameCatPrice.category}
+          </Typography>
+          <Typography className={classes.name}>
+            {props.relatedWithNameCatPrice.name}
+          </Typography>
+          <Typography className={classes.price}>
+            {props.relatedWithNameCatPrice.default_price}
+          </Typography>
+          <Typography>
+            <Rating defaultValue={3.5} precision={0.25} size="small"></Rating>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   )
 };
