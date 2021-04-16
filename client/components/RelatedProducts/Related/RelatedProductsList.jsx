e5288d7d9218c8c330c71b312a1dbf0fd37fa4b3
Amanda Davitt <acdavitt@gmail.com>
2021-04-16 18:58:40 -0500

    Added carousel from react-elastic-carousel

--- client/components/RelatedProducts/Related/RelatedProductsList.jsx
+++ client/components/RelatedProducts/Related/RelatedProductsList.jsx
@@ -3,14 +3,20 @@ import RelatedProductCard from './RelatedProductCard.jsx';
 import products from '../../DummyData/ProductDummyData';
 import GridList from '@material-ui/core/GridList';
 // import Carousel from 'react-material-ui-carousel'
+import Carousel from 'react-elastic-carousel';
 
+//CSS styling for later so disabled arrow does not show:
+// .rec.rec-arrow:disabled {
+//   visibility: hidden;
+// }
 
 const RelatedProductsList = (props) => {
 
   return  (
-    <GridList cols={4}>
-      {props.relatedWithNameCatPrice.map(item => <RelatedProductCard key={item.id} relatedWithNameCatPrice={item}/>)}
-    </GridList>
+    <Carousel showEmptySlots itemsToShow={4}>
+        {/* {products.products.map(item => <RelatedProductCard key={item.id} relatedWithNameCatPrice={item}/>)} */}
+        {props.relatedWithNameCatPrice.map(item => <RelatedProductCard key={item.id} relatedWithNameCatPrice={item}/>)}
+    </Carousel>
   )
 };
 
