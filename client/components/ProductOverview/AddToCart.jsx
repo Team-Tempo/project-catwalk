3d795f75060a39f6043c3cfe94a2355915afca6e
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-04-15 19:01:48 -0500

    Add placeholders for all Overview UI components

---
+++ client/components/ProductOverview/AddToCart.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  Grid,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Button,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const AddToCart = () => {
+  const classes = useStyles();
+  return (
+    <Grid container>
+      <Grid item xs={8}>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label">Size</InputLabel>
+          <Select labelId="demo-simple-select-label" id="demo-simple-select">
+            <MenuItem value={10}>XS</MenuItem>
+            <MenuItem value={20}>S</MenuItem>
+            <MenuItem value={30}>M</MenuItem>
+            <MenuItem value={40}>L</MenuItem>
+            <MenuItem value={50}>XL</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={4}>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label">Quantity</InputLabel>
+          <Select labelId="demo-simple-select-label" id="demo-simple-select">
+            <MenuItem value={10}>1</MenuItem>
+            <MenuItem value={20}>2</MenuItem>
+            <MenuItem value={30}>3</MenuItem>
+            <MenuItem value={40}>4</MenuItem>
+            <MenuItem value={50}>5</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={10}>
+        <Button
+          startIcon={<AddShoppingCartIcon />}
+          variant="contained"
+          color="primary"
+        >
+          <Typography>Add to cart</Typography>
+        </Button>
+      </Grid>
+      <Grid item xs={2}>
+        <Button variant="outlined">
+          <Typography>
+            <StarBorderIcon />
+          </Typography>
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AddToCart;
--- client/components/ProductOverview/ProductOverview.jsx
+++ client/components/ProductOverview/ProductOverview.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+import ImageGallery from './ImageGallery.jsx';
+import ProductInformation from './ProductInformation.jsx';
+import StyleSelector from './StyleSelector';
+import AddToCart from './AddToCart';
+
+import dummyData from '../DummyData/ProductDummyData';
+
+const getProduct = () => {
+  return axios
+    .get('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products', {
+      headers: {
+        Authorization: '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',
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      return response.data[0];
+    });
+};
+
+// const getProductStyles = () => {
+//   return axios.get(
+//     'https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products',
+//     {
+//       headers: {
+//         Authorization: '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',
+//       },
+//     }
+//   );
+// };
 
 const ProductOverview = () => {
-  return null;
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const result = await getProduct();
+      setProduct(result);
+    }
+    fetchProduct();
+  }, []);
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={8} justify="flex-end">
+        <ImageGallery dummyData={dummyData}></ImageGallery>
+      </Grid>
+      <Grid container direction="column" justify="space-between" item xs={4}>
+        <Grid item>
+          <ProductInformation product={product}></ProductInformation>
+        </Grid>
+        <Grid item>
+          <StyleSelector></StyleSelector>
+        </Grid>
+        <Grid item>
+          <AddToCart></AddToCart>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ProductOverview;
