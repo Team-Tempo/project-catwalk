0f13830fa6f6106bf9552177551fd0efd634f22e
Julia Thea <yuliya.boyadjan@gmail.com>
2021-04-16 18:22:50 -0500

    Implemented layout for rating breakdown

--- client/components/RatingsAndReviews/Rating/RatingSummaryStars.jsx
+++ client/components/RatingsAndReviews/Rating/RatingSummaryStars.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import Rating from '@material-ui/lab/Rating';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const RatingSummaryStars = () => {
   return (
     <>
-    <h1>3.5</h1>
-    <Rating name="read-only" readOnly />    </>
+    <Container>
+      <Grid container direction="row" spacing={1}>
+        <Grid item xs={3}><h1>3.5</h1></Grid>
+        <Grid item xs={4}>
+        <Rating name="read-only" readOnly />
+        </Grid>
+      </Grid>
+    </Container>
+    </>
   )
 }
 
--- client/components/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
 import ReviewsList from './Reviews/ReviewsList.jsx'
 import Rating from './Rating/Rating.jsx'
-import { Skeleton } from '@material-ui/lab';
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+  }
 }));
 
 const RatingsAndReviews = () => {
   const classes = useStyles();
   return (
     <>
-<div className={classes.root}>
+    <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={4}>
-          <Paper className={classes.paper}><p>Ratings and Reviews</p><Rating /></Paper>
+        <p>Ratings and Reviews</p><Rating />
         </Grid>
         <Grid item xs={8}>
-          <Paper className={classes.paper}><ReviewsList /></Paper>
+        <ReviewsList />
         </Grid>
       </Grid>
     </div>
