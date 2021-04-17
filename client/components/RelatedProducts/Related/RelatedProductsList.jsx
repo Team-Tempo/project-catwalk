9f3c09cf1305786d07af0c96e925ac8f2ef06f52
Amanda Davitt <acdavitt@gmail.com>
2021-04-17 11:52:49 -0500

    Destructed props and added Related Products title

--- client/components/RelatedProducts/Related/RelatedProductsList.jsx
+++ client/components/RelatedProducts/Related/RelatedProductsList.jsx
@@ -2,21 +2,26 @@ import React, {useState} from 'react';
 import RelatedProductCard from './RelatedProductCard.jsx';
 import products from '../../DummyData/ProductDummyData';
 import GridList from '@material-ui/core/GridList';
-// import Carousel from 'react-material-ui-carousel'
 import Carousel from 'react-elastic-carousel';
+import Typography from '@material-ui/core/Typography';
 
 //CSS styling for later so disabled arrow does not show:
 // .rec.rec-arrow:disabled {
 //   visibility: hidden;
 // }
 
-const RelatedProductsList = (props) => {
+const RelatedProductsList = ({relatedWithNameCatPrice}) => {
 
   return  (
-    <Carousel showEmptySlots itemsToShow={4}>
-        {/* {products.products.map(item => <RelatedProductCard key={item.id} relatedWithNameCatPrice={item}/>)} */}
-        {props.relatedWithNameCatPrice.map(item => <RelatedProductCard key={item.id} relatedWithNameCatPrice={item}/>)}
-    </Carousel>
+    <>
+      <Typography>
+        RELATED PRODUCTS
+      </Typography>
+      <Carousel showEmptySlots itemsToShow={4}>
+          {/* {products.products.map(item => <RelatedProductCard key={item.id} relatedWithNameCatPrice={item}/>)} */}
+          {relatedWithNameCatPrice.map(item => <RelatedProductCard key={item.id} relatedWithNameCatPrice={item}/>)}
+      </Carousel>
+    </>
   )
 };
 
