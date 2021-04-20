094580a55e29f041d8ca811b7b402065307fd51a
Amanda Davitt <acdavitt@gmail.com>
2021-04-19 22:18:55 -0500

    Refactored to avoid async issues

--- client/components/RelatedProducts/Related/RelatedProductCard.jsx
+++ client/components/RelatedProducts/Related/RelatedProductCard.jsx
@@ -7,8 +7,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import products from '../../DummyData/ProductDummyData';
-import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
 import Rating from '@material-ui/lab/Rating';
 
 
@@ -41,31 +40,34 @@ const useStyles = makeStyles({
 });
 
 
-const RelatedProductCard = ({nameCatPrice, salePic}) => {
+const RelatedProductCard = ({relatedProductsData}) => {
   const classes = useStyles();
+  const image = relatedProductsData.results[0].photos[0].url;
 
   return (
     <Card className={classes.root}>
       <CardActionArea >
         <CardMedia
         className={classes.media}
-        image={salePic.results[0].photos[0].url}
+        component="img"
+        image={image}
+        alt="Image not available"
         />
-        <IconButton className={classes.overlay}>
+        <Icon className={classes.overlay}>
           <span className="material-icons">star_rate</span>
-        </IconButton>
+        </Icon>
         <CardContent>
           {/*
           sale_price if on sale
           */}
           <Typography className={classes.category}>
-            {nameCatPrice.category}
+            {relatedProductsData.category}
           </Typography>
           <Typography className={classes.name}>
-            {nameCatPrice.name}
+            {relatedProductsData.name}
           </Typography>
           <Typography className={classes.price}>
-            {nameCatPrice.default_price}
+            {relatedProductsData.default_price}
           </Typography>
           <Typography>
             <Rating
--- client/components/RelatedProducts/RelatedProducts.jsx
+++ client/components/RelatedProducts/RelatedProducts.jsx
@@ -9,57 +9,48 @@ axios.defaults.headers.common['Authorization'] = config.GITHUB_TOKEN;
 
 const RelatedProducts = ({productId}) => {
 
+  const [relatedProductsData, setRelatedProductsData] = useState([]);
+
   async function getRelatedIds(productId) {
     const relatedIdsResponse = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${productId}/related`);
     return relatedIdsResponse.data;
   }
 
-  const [nameCatPrice, setNameCatPrice] = useState([]);
-  const [salePic, setSalePic] = useState([]);
-
   useEffect(() => {
     getRelatedIds(productId)
     .then(relatedIdsResult => {
 
-      const nameCatPriceGetReq = relatedIdsResult.map(id => {
+      const productsIdGetReq = relatedIdsResult.map(id => {
         return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}`);
       })
 
-      // const salePicGetReq = relatedIdsResult.map(id => {
-      //   return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}/styles`);
-      // })
-      return Promise.all(nameCatPriceGetReq);
-    })
-    .then(nameCatPriceResponse => nameCatPriceResponse.map(item => item.data))
-    .then(nameCatPriceResult => setNameCatPrice(nameCatPriceResult))
-    // .then(salePicResponse => {
+      const stylesGetReq = relatedIdsResult.map(id => {
+        return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}/styles`);
+      })
 
-    //   console.log({salePicResponse})
-    //   salePicResponse.map(item => item.data)
-    // })
-    // .then(salePicResult => setSalePic(salePicResult))
+      return Promise.all([...productsIdGetReq, ...stylesGetReq]);
+    })
+    .then(bothResponses => bothResponses.map(response => response.data))
+    .then(bothResponsesData => {
+
+      const midIndex = bothResponsesData.length / 2
+      const productsIdResult = bothResponsesData.slice(0, midIndex);
+      const stylesResult = bothResponsesData.slice(midIndex);
+      const combinedResults = [];
+
+      for (let i = 0; i < productsIdResult.length; i++) {
+        let temp = {...productsIdResult[i], ...stylesResult[i]};
+        combinedResults.push(temp);
+      }
+
+      setRelatedProductsData(combinedResults)
+    })
   }, [productId]);
-
-  useEffect(() => {
-    getRelatedIds(productId)
-    .then(relatedIdsResult => {
-
-      const salePicGetReq = relatedIdsResult.map(id => {
-      return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}/styles`);
-    })
-      return Promise.all(salePicGetReq);
-    })
-
-    .then(salePicResponse => salePicResponse.map(item => item.data))
-    .then(salePicResult => setSalePic(salePicResult))
-  }, [productId]);
-
 
   return (
     <div id='related'>
       <Grid container spacing={2}>
-      {console.log({salePic})}
-        <RelatedProductsList salePic={salePic} nameCatPrice={nameCatPrice} />
+        <RelatedProductsList relatedProductsData={relatedProductsData} />
       </Grid>
       <Grid>
         <CustomOutfitList />
