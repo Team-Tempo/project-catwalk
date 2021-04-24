af7c47b0a018fd87f66ece84e878b72e7fc23233
Julia Thea <yuliya.boyadjan@gmail.com>
2021-04-24 09:05:45 -0500

    Saving work

--- client/components/RatingsAndReviews/Reviews/AddReviewForm.jsx
+++ client/components/RatingsAndReviews/Reviews/AddReviewForm.jsx
@@ -7,38 +7,18 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-export default function SimpleRating() {
+export default function AddReviewForm() {
   const classes = useStyles();
+  const [rating, setRating] = useState(0);
+  const [reviewSummary, setReviewSummary] = useState('');
+  // const [] = useState();
+  // const [] = useState();
 
-  // const [rating, setRating] = useState(0);
-  // const [recommend, setRecommend] = useState();
-  // const [reviewSummary, setReviewSummary] = useState();
-  // const [reviewBody, setReviewBody] = useState();
-  // const [nickname, setNickname] = useState();
-  // const [email, setEmail] = useState();
+  function handleSummaryChange(event) {
+    console.log(event.target.value);
+    setReviewSummary(event.target.value);
+  }
 
-  // const initialState = {
-  //   rating: 3,
-  //   recommend: '',
-  //   reviewSummary: '',
-  //   reviewBody: '',
-  //   nickname: '',
-  //   email: '',
-  // };
-
-  // // Initializing state
-  // const [value, setValue] = useState(initialState);
-  // const { rating, recommend, reviewSummary, reviewBody, nickname, email } = value;
-
-  // common event handler for all the controlled components
-  // using spread operator to update the state
-
-  // const handleRatingChange = (event) => {
-  //   setValue({ ...value, [event.target.rating]: Number(event.target.value) })
-  // }
-  // const handleChange = (event) => {
-  //   setValue({ ...value, [event.target.name]: event.target.value })
-  // };
 
   return (
     <div>
@@ -47,23 +27,35 @@ export default function SimpleRating() {
       <Typography>Overall rating: </Typography>
         <Rating
           name="simple-controlled"
-          value={ rating } onChange={handleRatingChange}
+          value={ rating } onChange={(event, newRating) => {
+            console.log(event, newRating)
+            setRating(newRating);
+          }}
         />
       </Box>
       <Box component="fieldset" mb={3} borderColor="transparent">
       <label htmlFor="name">Do you recommend this product?&nbsp;&nbsp;&nbsp;</label>
-      <div class="form-check form-check-inline">
+      <div className="form-check form-check-inline">
       <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
-        <label className="form-check-label" for="inlineRadio1">Yes</label>
+        <label className="form-check-label" htmlFor="inlineRadio1">Yes</label>
       </div>
       <div className="form-check form-check-inline">
         <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
-        <label className="form-check-label" for="inlineRadio2">No</label>
+        <label className="form-check-label" htmlFor="inlineRadio2">No</label>
       </div>
       </Box>
       <Box component="fieldset" mb={3} borderColor="transparent">
       <label htmlFor="name">Review summary</label>
-    <input className="form-control" id="name" placeholder="Best purchase ever!" maxLength="60" onChange={console.log(value)}/>
+    <input
+      className="form-control"
+      placeholder="Best purchase ever!"
+      maxLength="60"
+      type="text"
+      name="reviewSummary"
+      value={ reviewSummary }
+      onChange={handleSummaryChange}
+
+      />
       </Box>
       <Box component="fieldset" mb={3} borderColor="transparent">
       <label htmlFor="name">Write your review here</label>
@@ -76,7 +68,7 @@ export default function SimpleRating() {
       </Box>
       <Box component="fieldset" className="form-group" mb={3} borderColor="transparent">
   <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
+    <label htmlFor="exampleInputEmail1">Email address</label>
     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" size="500"/>
     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
