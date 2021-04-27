b55f92701ae68c8f74583c796ec6bceb0eb37fee
Amanda Davitt <acdavitt@gmail.com>
2021-04-27 16:43:22 -0500

    Changed native alert to Snackbar when user tries to add duplicate outfit to CustomOutfitList

--- client/components/RelatedProducts/RelatedProducts.jsx
+++ client/components/RelatedProducts/RelatedProducts.jsx
@@ -4,6 +4,9 @@ import CustomOutfitList from './Outfit/CustomOutfitList.jsx';
 import Grid from '@material-ui/core/Grid';
 import axios from 'axios';
 import config from '../../../config.js'
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 axios.defaults.headers.common['Authorization'] = config.GITHUB_TOKEN;
 
@@ -17,6 +20,7 @@ const RelatedProducts = ({
 
   const [relatedProductsData, setRelatedProductsData] = useState([]);
   const [outfitCardsData, setOutfitCardsData] = useState(JSON.parse(localStorage.getItem('outfitCardsData')) || []);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
 
   async function getRelatedIds(productId) {
     const relatedIdsResponse = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${productId}/related`);
@@ -25,42 +29,52 @@ const RelatedProducts = ({
 
   useEffect(() => {
     getRelatedIds(productId)
-    .then(relatedIdsResult => {
-      const uniqueRelatedIdsSet = new Set(relatedIdsResult);
-      const uniqueRelatedIds = [...uniqueRelatedIdsSet]
+      .then(relatedIdsResult => {
+        const uniqueRelatedIdsSet = new Set(relatedIdsResult);
+        const uniqueRelatedIds = [...uniqueRelatedIdsSet]
 
-      const productsIdGetReq = uniqueRelatedIds.map(id => {
-        return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}`);
+        const productsIdGetReq = uniqueRelatedIds.map(id => {
+          return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}`);
+        })
+
+        const stylesGetReq = uniqueRelatedIds.map(id => {
+          return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}/styles`);
+        })
+
+        const ratingsGetReq = uniqueRelatedIds.map(id => {
+          return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews/meta?product_id=${id}`);
+        })
+
+        return Promise.all([...productsIdGetReq, ...stylesGetReq, ...ratingsGetReq]);
       })
+      .then(allResponses => allResponses.map(response => response.data))
+      .then(productCardData => {
+        //consolidates each product into one object within the combinedResults array:
+        const firstSplitIndex = productCardData.length / 3
+        const secondSplitIndex = firstSplitIndex * 2;
+        const productsIdResult = productCardData.slice(0, firstSplitIndex);
+        const stylesResult = productCardData.slice(firstSplitIndex, secondSplitIndex);
+        const ratingsResult = productCardData.slice(secondSplitIndex)
+        const combinedResults = [];
 
-      const stylesGetReq = uniqueRelatedIds.map(id => {
-        return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}/styles`);
+        for (let i = 0; i < productsIdResult.length; i++) {
+          let temp = { ...productsIdResult[i], ...stylesResult[i], ...ratingsResult[i] };
+          combinedResults.push(temp);
+        }
+
+        setRelatedProductsData(combinedResults)
       })
-
-      const ratingsGetReq = uniqueRelatedIds.map(id => {
-        return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews/meta?product_id=${id}`);
-      })
-
-      return Promise.all([...productsIdGetReq, ...stylesGetReq, ...ratingsGetReq]);
-    })
-    .then(allResponses => allResponses.map(response => response.data))
-    .then(productCardData => {
-      //consolidates each product into one object within the combinedResults array:
-      const firstSplitIndex = productCardData.length / 3
-      const secondSplitIndex = firstSplitIndex * 2;
-      const productsIdResult = productCardData.slice(0, firstSplitIndex);
-      const stylesResult = productCardData.slice(firstSplitIndex, secondSplitIndex);
-      const ratingsResult = productCardData.slice(secondSplitIndex)
-      const combinedResults = [];
-
-      for (let i = 0; i < productsIdResult.length; i++) {
-        let temp = {...productsIdResult[i], ...stylesResult[i], ...ratingsResult[i]};
-        combinedResults.push(temp);
-      }
-
-      setRelatedProductsData(combinedResults)
-    })
   }, [productId]);
+
+  //click handlers for Snackbar
+  const handleSnackbarOpen = () => {
+    setOpenSnackbar(true);
+  };
+
+  const handleSnackbarClose = () => {
+    setOpenSnackbar(false);
+  };
+
 
   const addToOutfit = () => {
     let isPresentInOutfit = false;
@@ -86,7 +100,7 @@ const RelatedProducts = ({
       setOutfitCardsData(outfitCardsDataCopy);
       localStorage.setItem('outfitCardsData', JSON.stringify(outfitCardsDataCopy));
     } else {
-      alert('This style has already been added to your outfit list!')
+      handleSnackbarOpen();
     }
   }
 
@@ -109,11 +123,29 @@ const RelatedProducts = ({
     <div id='related'>
       <Grid container spacing={2}>
         <RelatedProductsList setProductId={setProductId} relatedProductsData={relatedProductsData} product={product} />
+        <Snackbar
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          autoHideDuration={6000}
+          open={openSnackbar}
+          onClose={handleSnackbarClose}
+          message="This style has already been added to your outfit list!"
+          action={
+            <IconButton
+              size="medium"
+              aria-label="close"
+              color="inherit"
+              onClick={handleSnackbarClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          }
+        >
+        </Snackbar>
       </Grid>
       <Grid>
         <CustomOutfitList outfitCardsData={outfitCardsData} addToOutfit={addToOutfit} removeCard={removeCard} />
       </Grid>
-     </div>
+    </div>
   );
 };
 
