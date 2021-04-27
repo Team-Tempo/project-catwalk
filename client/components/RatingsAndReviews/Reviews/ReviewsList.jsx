9e6fc7d1d84089f7f584c88d660a8ef6119daa2f
Julia Thea <yuliya.boyadjan@gmail.com>
2021-04-27 10:14:18 -0500

    Saving work

--- client/components/RatingsAndReviews/Reviews/ReviewsList.jsx
+++ client/components/RatingsAndReviews/Reviews/ReviewsList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Review from './Review.jsx';
 import AddReviewDialog from './AddReviewDialog';
-import { Typography, Grid, makeStyles } from '@material-ui/core'
+import { Typography, Grid, GridList, GridListTile, makeStyles } from '@material-ui/core'
 import axios from 'axios';
 import config from '../../../../config.js';
 axios.defaults.headers.common['Authorization'] = config.GITHUB_TOKEN
@@ -14,6 +14,17 @@ const getReviewsData = async (id) => {
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  scroll: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'auto',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    // width: 1000,
+    height: 700,
   },
   header: {
     fontWeight: 500,
@@ -39,10 +50,27 @@ const ReviewsList = ({ productId }) => {
         <Grid item xs={12}>
           <Typography className={classes.header}>{ reviewsCounter } reviews, sorted by relevance</Typography>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12}  className={classes.scroll}>
+
+
+
+        {/* <div className={classes.scroll}> */}
+      <GridList cellHeight={140} className={classes.gridList} cols={1}>
+
+
           {reviewsData.map(review =>
+      //  <GridListTile key={review.review_id} cols={review.cols || 1}>
+
           <Review key={review.review_id} review={review}/>
-        )}
+
+          )}
+       {/* </GridListTile> */}
+
+
+      </GridList>
+          {/* </div> */}
+
+
         </Grid>
         <Grid item xs={12} className={classes.buttons}>
           <AddReviewDialog productId={productId}/>
