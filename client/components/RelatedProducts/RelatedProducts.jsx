bdcc37659fbfe69607c737f9924aa65806f9aa7f
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-04-28 10:45:41 -0500

    Began setup

--- client/components/RelatedProducts/RelatedProducts.jsx
+++ client/components/RelatedProducts/RelatedProducts.jsx
@@ -3,67 +3,88 @@ import RelatedProductsList from './Related/RelatedProductsList.jsx';
 import CustomOutfitList from './Outfit/CustomOutfitList.jsx';
 import Grid from '@material-ui/core/Grid';
 import axios from 'axios';
-import config from '../../../config.js'
+import dotenv from 'dotenv';
+
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
-axios.defaults.headers.common['Authorization'] = config.GITHUB_TOKEN;
+axios.defaults.headers.common['Authorization'] = process.env.GITHUB_TOKEN;
 
 const RelatedProducts = ({
   productId,
   product,
   currentStyle,
   averageRating,
-  setProductId
+  setProductId,
 }) => {
-
   const [relatedProductsData, setRelatedProductsData] = useState([]);
-  const [outfitCardsData, setOutfitCardsData] = useState(JSON.parse(localStorage.getItem('outfitCardsData')) || []);
+  const [outfitCardsData, setOutfitCardsData] = useState(
+    JSON.parse(localStorage.getItem('outfitCardsData')) || []
+  );
   const [openSnackbar, setOpenSnackbar] = useState(false);
 
   async function getRelatedIds(productId) {
-    const relatedIdsResponse = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${productId}/related`);
+    const relatedIdsResponse = await axios.get(
+      `https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${productId}/related`
+    );
     return relatedIdsResponse.data;
   }
 
   useEffect(() => {
     getRelatedIds(productId)
-      .then(relatedIdsResult => {
+      .then((relatedIdsResult) => {
         const uniqueRelatedIdsSet = new Set(relatedIdsResult);
-        const uniqueRelatedIds = [...uniqueRelatedIdsSet]
+        const uniqueRelatedIds = [...uniqueRelatedIdsSet];
 
-        const productsIdGetReq = uniqueRelatedIds.map(id => {
-          return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}`);
-        })
+        const productsIdGetReq = uniqueRelatedIds.map((id) => {
+          return axios.get(
+            `https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}`
+          );
+        });
 
-        const stylesGetReq = uniqueRelatedIds.map(id => {
-          return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}/styles`);
-        })
+        const stylesGetReq = uniqueRelatedIds.map((id) => {
+          return axios.get(
+            `https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${id}/styles`
+          );
+        });
 
-        const ratingsGetReq = uniqueRelatedIds.map(id => {
-          return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews/meta?product_id=${id}`);
-        })
+        const ratingsGetReq = uniqueRelatedIds.map((id) => {
+          return axios.get(
+            `https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews/meta?product_id=${id}`
+          );
+        });
 
-        return Promise.all([...productsIdGetReq, ...stylesGetReq, ...ratingsGetReq]);
+        return Promise.all([
+          ...productsIdGetReq,
+          ...stylesGetReq,
+          ...ratingsGetReq,
+        ]);
       })
-      .then(allResponses => allResponses.map(response => response.data))
-      .then(productCardData => {
+      .then((allResponses) => allResponses.map((response) => response.data))
+      .then((productCardData) => {
         //consolidates each product into one object within the combinedResults array:
-        const firstSplitIndex = productCardData.length / 3
+        const firstSplitIndex = productCardData.length / 3;
         const secondSplitIndex = firstSplitIndex * 2;
         const productsIdResult = productCardData.slice(0, firstSplitIndex);
-        const stylesResult = productCardData.slice(firstSplitIndex, secondSplitIndex);
-        const ratingsResult = productCardData.slice(secondSplitIndex)
+        const stylesResult = productCardData.slice(
+          firstSplitIndex,
+          secondSplitIndex
+        );
+        const ratingsResult = productCardData.slice(secondSplitIndex);
         const combinedResults = [];
 
         for (let i = 0; i < productsIdResult.length; i++) {
-          let temp = { ...productsIdResult[i], ...stylesResult[i], ...ratingsResult[i] };
+          let temp = {
+            ...productsIdResult[i],
+            ...stylesResult[i],
+            ...ratingsResult[i],
+          };
           combinedResults.push(temp);
         }
 
-        setRelatedProductsData(combinedResults)
-      })
+        setRelatedProductsData(combinedResults);
+      });
   }, [productId]);
 
   //click handlers for Snackbar
@@ -74,7 +95,6 @@ const RelatedProducts = ({
   const handleSnackbarClose = () => {
     setOpenSnackbar(false);
   };
-
 
   const addToOutfit = () => {
     let isPresentInOutfit = false;
@@ -94,15 +114,18 @@ const RelatedProducts = ({
         photo: currentStyle.photos[0].thumbnail_url,
         style_id: currentStyle.style_id,
         category: product.category,
-        rating: averageRating
-      }
+        rating: averageRating,
+      };
       outfitCardsDataCopy.push(outfitCardData);
       setOutfitCardsData(outfitCardsDataCopy);
-      localStorage.setItem('outfitCardsData', JSON.stringify(outfitCardsDataCopy));
+      localStorage.setItem(
+        'outfitCardsData',
+        JSON.stringify(outfitCardsDataCopy)
+      );
     } else {
       handleSnackbarOpen();
     }
-  }
+  };
 
   const removeCard = (styleId) => {
     let indexOfCardToRemove;
@@ -116,13 +139,20 @@ const RelatedProducts = ({
     outfitCardsData.splice(indexOfCardToRemove, 1);
     let removedOutfitCardsData = outfitCardsData.slice();
     setOutfitCardsData(removedOutfitCardsData);
-    localStorage.setItem('outfitCardsData', JSON.stringify(removedOutfitCardsData));
-  }
+    localStorage.setItem(
+      'outfitCardsData',
+      JSON.stringify(removedOutfitCardsData)
+    );
+  };
 
   return (
-    <div id='related'>
+    <div id="related">
       <Grid container spacing={2}>
-        <RelatedProductsList setProductId={setProductId} relatedProductsData={relatedProductsData} product={product} />
+        <RelatedProductsList
+          setProductId={setProductId}
+          relatedProductsData={relatedProductsData}
+          product={product}
+        />
         <Snackbar
           anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
           autoHideDuration={6000}
@@ -139,11 +169,14 @@ const RelatedProducts = ({
               <CloseIcon fontSize="small" />
             </IconButton>
           }
-        >
-        </Snackbar>
+        ></Snackbar>
       </Grid>
       <Grid>
-        <CustomOutfitList outfitCardsData={outfitCardsData} addToOutfit={addToOutfit} removeCard={removeCard} />
+        <CustomOutfitList
+          outfitCardsData={outfitCardsData}
+          addToOutfit={addToOutfit}
+          removeCard={removeCard}
+        />
       </Grid>
     </div>
   );
